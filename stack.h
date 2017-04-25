26be61a5c5c621d8c5845b824045bcdfa8a4354d
arvind <amahankali@stuy.edu>
2017-04-25 15:15:19 -0400

    got base

---
+++ stack.h
@@ -0,0 +1,19 @@
+#ifndef STACK_H
+#define STACK_H
+
+#define STACK_SIZE 2
+
+struct stack {
+  int size;
+  int top;
+  struct matrix **data;
+};
+
+struct stack * new_stack();
+void push( struct stack *s );
+void pop(struct stack *s);
+
+void free_stack( struct stack *);
+void print_stack( struct stack *);
+
+#endif
